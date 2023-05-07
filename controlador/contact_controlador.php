68e0adb4d8c86dac4f8e6debbbb6ca338874f0e8
lilgbexx <118175971+lilgbexx@users.noreply.github.com>
2023-05-07 22:32:14 +0200

    Add files via upload

---
+++ controlador/contact_controlador.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+function home(){
+    require_once('vista/contact_vista.php');
+}
+
+?>
---
+++ js/script.js
@@ -0,0 +1,32 @@
+var indice; 
+function modificar(id) {
+    $.ajax({
+        type: 'POST',
+        data: {'usuario': document.getElementById("usuarioD"+id).value,
+        'nombre1': document.getElementById("nombreD"+id).value,
+        'apellido1': document.getElementById("apellidoD"+id).value,
+        'correo1': document.getElementById("correoD"+id).value,
+        'direccion1': document.getElementById("direccionD"+id).value,
+        'telefono1': document.getElementById("telefonoD"+id).value},
+
+        url: 'controlador/datos_controlador.php',
+        success: function (response) {
+            document.getElementById("insertForm").style.display = 'none';
+            document.getElementById("container").style.display = 'inherit';
+            $("#container").html(response); //respuesta formulario
+        }
+    })
+}
+
+
+
+
+
+
+
+
+function cancelar() {
+    document.getElementById("insertForm").style.display = 'inherit';
+    document.getElementById("container").style.display = 'none';
+}
+
